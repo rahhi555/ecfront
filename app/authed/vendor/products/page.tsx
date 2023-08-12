6d53e5583fca80496ce169bdab1a62027b870852
rahhi555 <68116815+rahhi555@users.noreply.github.com>
2023-08-12 19:01:28 +0900

    add: table

--- app/authed/vendor/products/page.tsx
+++ app/authed/vendor/products/page.tsx
@@ -1,16 +1,25 @@
 import { ProductList } from "@/components/product-list";
 import { $path } from "@/generate/path";
 import { Link } from "@nextui-org/link";
-import { Suspense } from "react";
+import { Button } from "@nextui-org/button";
+import { MdAdd } from "react-icons/md";
+import { Title } from "@/components/title";
 
 export default function VendorProductsPage() {
   return (
     <div>
-      <h1>Products</h1>
-      <Link href={$path("/authed/vendor/products/new")}>new product</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <ProductList />
-      </Suspense>
+      <Title title="商品一覧" />
+
+      <Button
+        href={$path("/authed/vendor/products/new")}
+        as={Link}
+        color="primary"
+        startContent={<MdAdd />}
+        className="mb-4"
+      >
+        商品追加
+      </Button>
+      <ProductList />
     </div>
   );
 }
--- components/product-list.tsx
+++ components/product-list.tsx
@@ -1,11 +1,66 @@
-import { serverApi } from "@/hooks/useApi";
+"use client";
 
-export async function ProductList() {
-  const { data: products } = await serverApi().GET("/products", {});
+import { useClientApi } from "@/hooks/useApi";
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  getKeyValue,
+} from "@nextui-org/table";
+import { useEffect, useState } from "react";
+import type { components } from "@/generate/openapi-type";
+import { Spinner } from "@nextui-org/spinner";
+import { useAsyncList } from "@react-stately/data";
+
+type DTOProduct = components["schemas"]["DTOProduct"];
+
+const columns = [
+  { key: "id", label: "id" },
+  { key: "name", label: "商品名" },
+  { key: "description", label: "説明" },
+  { key: "price", label: "価格" },
+  { key: "stock", label: "在庫数" },
+  { key: "createdAt", label: "作成日" },
+];
+
+export function ProductList() {
+  const { GET } = useClientApi();
+  const [loading, setIsLoading] = useState(true);
+
+  const products = useAsyncList<DTOProduct>({
+    async load({ signal }) {
+      const { data } = await GET("/products", { signal });
+      setIsLoading(false);
+      return {
+        items: data || [],
+      };
+    },
+  });
 
   return (
-    <ul>
-      {products?.map((product) => <li key={product.id}>{product.name}</li>)}
-    </ul>
+    <Table>
+      <TableHeader>
+        {columns.map((column) => (
+          <TableColumn key={column.key}>{column.label}</TableColumn>
+        ))}
+      </TableHeader>
+
+      <TableBody
+        items={products.items}
+        isLoading={loading}
+        loadingContent={<Spinner label="Loading..." />}
+      >
+        {(product) => (
+          <TableRow key={product.id}>
+            {(columnKey) => (
+              <TableCell>{getKeyValue(product, columnKey)}</TableCell>
+            )}
+          </TableRow>
+        )}
+      </TableBody>
+    </Table>
   );
 }
